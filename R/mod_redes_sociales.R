74e47895503c95aab97c48f539ae482a3cd43bf7
hectorchavezgaro44 <hector.chaga94@gmail.com>
2021-02-24 14:46:49 -0600

    gráficas en pestaña de redes sociales

--- R/mod_redes_sociales.R
+++ R/mod_redes_sociales.R
@@ -7,9 +7,26 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
+#' @import dplyr highcharter ggplot2 tibble
 mod_redes_sociales_ui <- function(id){
   ns <- NS(id)
   tagList(
+    fluidRow(
+      column(width = 12, 
+             selectInput(ns("candidato"), label = "Selecciones candidato", choices = c("Candidato 1", "Candidato 2", "Candidato 3"), selected = "Candidato 1")
+      )
+    ),
+    fluidRow(
+      column(width = 12,class="shadowBox",
+             highchartOutput(ns("reach"))
+      )
+    ), 
+
+    fluidRow(
+      column(width = 12, class="shadowBox",
+             highchartOutput(ns("saldo"))
+      )
+    )
  
   )
 }
@@ -19,6 +36,66 @@ mod_redes_sociales_ui <- function(id){
 #' @noRd 
 mod_redes_sociales_server <- function(input, output, session){
   ns <- session$ns
+  
+  
+  output$reach <- renderHighchart({
+    tempo1 <- tibble( grupo = sample(c("Tweet", "Menciones", "RT", "Likes"),
+                                        prob = c(.2,.2 ,.3, .3), size = 100, replace = T),
+                         fecha = sample(seq(today()-100, today(), length.out = 11), size = 100, replace = T ), 
+                         candidato = "Candidato 1")
+    tempo2 <- tibble( grupo = sample(c("Tweet", "Menciones", "RT", "Likes"),
+                                     prob = c(.2,.2 ,.3, .3), size = 100, replace = T),
+                      fecha = sample(seq(today()-100, today(), length.out = 11), size = 100, replace = T ), 
+                      candidato = "Candidato 2")
+    
+    tempo3 <- tibble( grupo = sample(c("Tweet", "Menciones", "RT", "Likes"),
+                                     prob = c(.2,.2 ,.3, .3), size = 100, replace = T),
+                      fecha = sample(seq(today()-100, today(), length.out = 11), size = 100, replace = T ), 
+                      candidato = "Candidato 3")
+      proyectos <- bind_rows(tempo1, tempo2)
+      
+      proyectos <- bind_rows(proyectos, tempo3) %>% 
+      filter(candidato==!!input$candidato) %>% 
+      arrange(fecha) %>% 
+      group_by(fecha, grupo) %>%
+      summarise(n=n()) 
+    
+    reach(proyectos)
+  })
+  
+  output$saldo <- renderHighchart({
+    tempo1 <- tibble(votos = sample(30:78, size = 172, replace = T),
+                     voto = sample(c("Negativo", "Positivo"), size = 172, replace = T, prob = c(.5, .5)), 
+                     candidato = "Candidato 1") %>%
+      mutate(mes = cut(votos,c(17,29,39,49,59,69,79, 89,100),
+                       labels = c("Semana 1", "Semana 2", "Semana 3", "Semana 4", "Semana 5", "Semana 6", "Semana 7", "Semana 8"))) 
+    
+    tempo2 <- tibble(votos = sample(30:78, size = 172, replace = T),
+                     voto = sample(c("Negativo", "Positivo"), size = 172, replace = T, prob = c(.5, .5)), 
+                     candidato = "Candidato 2") %>%
+      mutate(mes = cut(votos,c(17,29,39,49,59,69,79, 89,100),
+                       labels = c("Semana 1", "Semana 2", "Semana 3", "Semana 4", "Semana 5", "Semana 6", "Semana 7", "Semana 8"))) 
+    
+    
+    tempo3 <- tibble(votos = sample(30:78, size = 172, replace = T),
+                     voto = sample(c("Negativo", "Positivo"), size = 172, replace = T, prob = c(.5, .5)), 
+                     candidato = "Candidato 3") %>%
+      mutate(mes = cut(votos,c(17,29,39,49,59,69,79, 89,100),
+                       labels = c("Semana 1", "Semana 2", "Semana 3", "Semana 4", "Semana 5", "Semana 6", "Semana 7", "Semana 8"))) 
+    
+    df <- bind_rows(tempo1, tempo2)
+    
+    df <- bind_rows(df, tempo3)%>%
+      filter(candidato==!!input$candidato) %>% 
+      count(mes, voto) %>%
+      mutate(n = as.double(n),
+             n2= case_when(voto == "Negativo"~ n*-1,
+                           voto == "Positivo"~ n))
+    
+    graficando_saldo(df)
+  })
+  
+  
  
 }
     
