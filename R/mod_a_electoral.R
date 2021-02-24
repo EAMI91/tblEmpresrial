4d711fcc8178b8e9849e9db015310b09e1b16a2e
EAMI91 <emiliomorones@gmail.com>
2021-02-24 12:32:12 -0600

    se ve mejor

--- R/mod_a_electoral.R
+++ R/mod_a_electoral.R
@@ -12,27 +12,27 @@ mod_a_electoral_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      slickROutput(ns("pdf"),width='80%',height = 100) 
+      slickROutput(ns("pdf"),width='90%',height = 100) 
     )
   )
 }
-    
+
 #' a_electoral Server Function
 #'
 #' @noRd 
 mod_a_electoral_server <- function(input, output, session){
   ns <- session$ns
- 
+  
   output$pdf <- renderSlickR({
     slickR(
-      imgs,height = 800
+      imgs,height = 800,width = "100%",
     )
   }) 
 }
-    
+
 ## To be copied in the UI
 # mod_a_electoral_ui("a_electoral_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_a_electoral_server, "a_electoral_ui_1")
- 
+
