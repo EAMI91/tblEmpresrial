c652453a9da7a2e095af5723639e6820f7fda58c
EAMI91 <emiliomorones@gmail.com>
2021-03-10 09:52:13 -0600

    perfil de candidatos

--- R/mod_candidatos.R
+++ R/mod_candidatos.R
@@ -22,9 +22,9 @@ mod_candidatos_server <- function(input, output, session, entidad){
   
   bd <- reactive({
     imgs_cand %>% filter(Entidad == !!entidad()) %>% 
-      mutate(img = glue::glue("inst/{entidad()}/Gobernador/{`Foto formal`}"),
-             nacimiento = glue::glue("{`Lugar de nacimiento`} ({format(`Fecha de nacimiento`,'%d %B %y')})"),
-             texto = paste(Candidato,Partido,nacimiento,Estudios,`Cargo actual`,`Perfil político`,sep = "\n"))
+      mutate(img = glue::glue("inst/{entidad()}/Gobernador/{foto_perfil}"))#,
+             # nacimiento = glue::glue("{`Lugar de nacimiento`} ({format(`Fecha de nacimiento`,'%d %B %y')})"),
+             # texto = paste(Candidato,Partido,nacimiento,Estudios,`Cargo actual`,`Perfil político`,sep = "\n"))
   })
   
   output$candidatos <- renderSlickR({
@@ -32,25 +32,25 @@ mod_candidatos_server <- function(input, output, session, entidad){
       need(nrow(bd())>0,message = "No se ha registrado ningún candidato para esta entidad.")
     )
     imagen <- slickR(
-      bd()$img,height = 500,width = "100%",slideId = "candidatos",
+      bd()$img,height = 700,width = "100%",slideId = "candidatos",
     ) + settings(autoplay = T, autoplaySpeed = 5000)
-    texto <- slickR(elementId = "nombre",bd()$texto,slideType = "p",slideId  = "candidatos2") + settings(dots = T, arrows = F)
+    # texto <- slickR(elementId = "nombre",bd()$texto,slideType = "p",slideId  = "candidatos2") + settings(dots = T, arrows = F)
     # partido <- slickR(bd()$Partido,slideType = "p") + settings(arrows = F)
     # nacimiento <- slickR(bd()$nacimiento,slideType = "p") + settings(arrows = F)
     # estudios <- slickR(bd()$Estudios,slideType = "p") + settings(arrows = F)
     # cargo <- slickR(bd()$`Cargo actual`,slideType = "p") + settings(arrows = F)
     # perfil <- slickR(bd()$`Perfil político`,slideType = "p") + settings(arrows = F)
     
-    lista <- list(imagen,
-                  texto)
+    # lista <- list(imagen,
+    #               texto)
                   # nombre,
                   # partido,
                   # nacimiento,
                   # estudios,
                   # cargo,
                   # perfil)
-    
-    Reduce(`%synch%`, lista)
+    imagen
+    # Reduce(`%synch%`, lista)
     # a <- imagen %synch% nombre
     # b <- partido %synch% nacimiento
     # imagen %stack% nombre %stack% partido
--- data-raw/imgs_cand.R
+++ data-raw/imgs_cand.R
@@ -1,5 +1,6 @@
 ## code to prepare `imgs_cand` dataset goes here
 
 library(readr)
+library(dplyr)
 imgs_cand <- read_csv("data-raw/aspirantes_prueba_mich_qro_nl.csv") %>% mutate(`Fecha de nacimiento` = lubridate::dmy(`Fecha de nacimiento`))
 usethis::use_data(imgs_cand, overwrite = TRUE)
